24f90c347155017be193fa6e1e9c8202b6bac5de
Tushar-innogent <Tushar.patidar@innogent.in>
2024-05-21 16:22:42 +0530

    Initial commit

---
+++ src/main/java/com/innogent/PMS/PmsApplication.java
@@ -0,0 +1,13 @@
+package com.innogent.PMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsApplication.class, args);
+	}
+
+}
