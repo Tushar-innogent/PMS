d8164bb64682907a4cb55056e841f3f0c287d9fa
Anjali Padmor <anjalipadmor@gmail.com>
2024-05-21 16:31:41 +0530

    in main file

--- src/main/java/com/innogent/PMS/PmsApplication.java
+++ src/main/java/com/innogent/PMS/PmsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PmsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PmsApplication.class, args);
+		System.out.println("performance manager");
 	}
 
 }
