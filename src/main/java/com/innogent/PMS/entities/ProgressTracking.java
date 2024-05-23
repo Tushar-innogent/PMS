03dea6b77410043913ec421ef4c3572944640c07
jhefjs <tanish.sugandhi1608@gmail.com>
2024-05-23 19:16:04 +0530

    Working on different apis

---
+++ src/main/java/com/innogent/PMS/entities/ProgressTracking.java
@@ -0,0 +1,27 @@
+package com.innogent.PMS.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name="progress_trackings")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgressTracking {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+   private Long meetingId;
+    @CreationTimestamp
+   private LocalDateTime date;
+   private String notes;
+   private String recording;
+}
