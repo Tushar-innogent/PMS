5054bce061127b89f3607a4cc902180ba90d4e13
Tushar-innogent <Tushar.patidar@innogent.in>
2024-05-27 17:52:59 +0530

    goal module updates

--- src/main/java/com/innogent/PMS/enums/StageName.java
+++ src/main/java/com/innogent/PMS/enums/StageName.java
@@ -3,5 +3,6 @@ package com.innogent.PMS.enums;
 public enum StageName {
     GOAL_SETTING,
     PROGRESS_TRACKING,
-    FEEDBACK_COLLECTION
+    FEEDBACK_COLLECTION,
+    PERFORMANCE_REVIEW
 }
--- src/main/java/com/innogent/PMS/enums/StageStatus.java
+++ src/main/java/com/innogent/PMS/enums/StageStatus.java
@@ -2,6 +2,7 @@ package com.innogent.PMS.enums;
 
 public enum StageStatus {
     PENDING,
+    FINALISED,
     IN_PROGRESS,
     COMPLETED;
 }
