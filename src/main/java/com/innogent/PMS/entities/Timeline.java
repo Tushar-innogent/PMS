d87b15ac2a55e2d77aca044af300273cf4b8e656
Anjali Padmor <anjalipadmor@gmail.com>
2024-07-02 19:23:35 +0530

    add one entity TimelineCycle for storing yearly dates

--- src/main/java/com/innogent/PMS/entities/Timeline.java
+++ src/main/java/com/innogent/PMS/entities/Timeline.java
@@ -1,6 +1,42 @@
+//package com.innogent.PMS.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "timelines")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//public class
+//
+//Timeline {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer timelineId;
+//    private LocalDateTime startDate;
+//    private LocalDateTime endDate;
+//
+//    private String description;
+//    private Boolean isActive;
+//
+//    @PrePersist
+//    protected void onCreate() {
+//        if (isActive == null) {
+//            isActive = true;
+//        }
+//    }
+//
+//@Column(name = "stage_id")
+//private Long stageId;
+//}
+
 package com.innogent.PMS.entities;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,9 +49,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class
-
-Timeline {
+public class Timeline {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer timelineId;
@@ -32,6 +66,10 @@ Timeline {
         }
     }
 
-@Column(name = "stage_id")
-private Long stageId;
+    @Column(name = "stage_id")
+    private Long stageId;
+
+    @ManyToOne(cascade= CascadeType.MERGE)
+    @JoinColumn(name = "timeline_cycle_id", referencedColumnName = "timelineCycleId")
+    private TimelineCycle timelineCycle;
 }
